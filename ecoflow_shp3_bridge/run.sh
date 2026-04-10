9b19e8ca2da7530e3f1a1a9615fe5f8757c1ae87
MatthewJB2022 <116841503+MatthewJB2022@users.noreply.github.com>
2026-04-10 17:33:27 -0400

    Change shebang and remove export statements
    Updated shebang to use '/usr/bin/env bash' and removed export statements for configuration variables.

--- ecoflow_shp3_bridge/run.sh
+++ ecoflow_shp3_bridge/run.sh
@@ -1,11 +1,5 @@
-#!/usr/bin/with-contenv bashio
+#!/usr/bin/env bash
 
 echo "Starting EcoFlow SHP3 Bridge..."
-
-export MQTT_HOST=$(bashio::config 'mqtt_host')
-export MQTT_PORT=$(bashio::config 'mqtt_port')
-export MQTT_USER=$(bashio::config 'mqtt_user')
-export MQTT_PASS=$(bashio::config 'mqtt_pass')
-export UPDATE_INTERVAL=$(bashio::config 'update_interval')
 
 python3 /app/main.py
